0f49b18028c601bbb0e76adfac3978d413f78af2
anushka-code <62592095+anushka-code@users.noreply.github.com>
2020-04-09 19:19:54 +0530

    Add files via upload
    This is the fitness tool for our app PCOS Virago (one of the major features of our app)

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/screens/input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xfffababa),
+        scaffoldBackgroundColor: Color(0xff241F25),
+      ),
+      home: InputPage(),
+    );
+  }
+}
